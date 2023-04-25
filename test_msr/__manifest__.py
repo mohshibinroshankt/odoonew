1787c1dea08dc95d4331be84a2699557e26a2ca9
mohshibinroshankt <mohammedshibinroshan@gmail.com>
2023-04-25 10:44:39 +0530

    Added custom module

---
+++ test_msr/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'test msr',
+    'website': 'machinser.com',
+    'summary': 'wewwww',
+    'author': 'msrkt',
+    'depends': ['sale', 'base'],
+    'data': [
+        'views/sale_order_view.xml',
+        'views/sale_order_report_view.xml',
+        'views/sale_order_tree_view.xml',
+
+    ],
+    'demo': []
+}
