4b866cfeee1477eed8c5a50b5aa112d35bd40904
mohshibinroshankt <mohammedshibinroshan@gmail.com>
2023-04-26 13:34:32 +0530

    latest1

--- sale_delivery_msr/models/sale_order.py
+++ sale_delivery_msr/models/sale_order.py
@@ -6,31 +6,37 @@ class SaleOrder(models.Model):
 
     delivery_charges = fields.Float(string='Delivery Charges')
 
-    @api.onchange('delivery_charges')
-    def onchange_delivery_charges(self):
-        if self.delivery_charges:
-            last_order_line = self.order_line.sorted(key=lambda l: l.sequence, reverse=True)[0]
+      @api.onchange('delivery_charges')
+        def onchange_delivery_charges(self):
+            if self.delivery_charges > 0:
+                delivery_charges_product = self.env['product.product'].search([('name', '=', 'Delivery Charges')],
+                                                                              limit=1)
 
-            delivery_charges_product = self.env['product.product'].create({
-                'name': 'Delivery Charges',
-                'list_price': self.delivery_charges,
-            })
+                if not delivery_charges_product:
+                    delivery_charges_product = self.env['product.product'].create({
+                        'name': 'Delivery Charges',
+                        'list_price': self.delivery_charges,
+                        'type': 'service',
+                    })
 
-            # dictionary to hold the values
-            order_line_vals = {
-                'order_id': self.id,
-                'name': 'Delivery Charges',
-                'product_id': delivery_charges_product.id,
-                'product_uom_qty': 1.0,
-                'price_unit': delivery_charges_product.list_price,
-                'sequence': last_order_line.sequence + 1,
-            }
+                order_line = self.order_line.filtered(lambda l: l.product_id == delivery_charges_product)
 
-            new_order_line = self.env['sale.order.line'].new(order_line_vals)
+                if order_line:
+                    # updating price_unit of existing order line
+                    order_line.write({'price_unit': self.delivery_charges})
+                else:
+                    # creating a new order line with updated price_unit
+                    last_order_line = self.order_line.sorted(key=lambda l: l.sequence, reverse=True)[0]
+                    order_line_vals = {
+                        'order_id': self.id,
+                        'name': 'Delivery Charges',
+                        'product_id': delivery_charges_product.id,
+                        'product_uom_qty': 1.0,
+                        'price_unit': self.delivery_charges,
+                        'sequence': last_order_line.sequence + 1,
+                    }
+
+                    new_order_line = self.env['sale.order.line'].new(order_line_vals)
 
 
-<<<<<<< HEAD
     
-=======
-   
->>>>>>> 6d250053c7760b508fbeb294d9e07df76dad65f9
